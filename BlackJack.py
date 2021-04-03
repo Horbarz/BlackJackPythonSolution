434a8f3a2c83261e8fabf152eed2be31d328fe33
horbarz <horbarz007@gmail.com>
2021-04-03 16:45:05 +0100

    Algorithm updated

--- BlackJack.py
+++ BlackJack.py
@@ -1,10 +1,15 @@
 import random
 cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
-card_point = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10,"A":11}
+card_point = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10,"A":11,"A1":1}
 deck = []
 for i in range(0,4):
     for card in cards:
         deck.append(card)
+
+def optimalAce(card,newVal):
+    if("A" in card and newVal=="A"):
+        return "A1"
+    return newVal
 
 def sumScores(cards):
     global card_point
@@ -38,15 +43,17 @@ def checkHumanBust(humanScore):
         quit()
 
 def checkDealerBust(dealerScore, humanScore, humanCards, dealerCards):
-    if dealerScore == 17 and humanScore<21:
-        print("Player wins!" + "\n"+showCards(humanCards)+" = " + str(humanScore) + " to Dealer's "+showCards(dealerCards) + " = " + str(dealerScore))
+    if dealerScore == humanScore:
+        print("TIE!")
         quit()
-    elif dealerScore == 17 and humanScore>21:
-        print("Player busts with " + str(humanScore) + "\nDealer wins")
-        quit()
-    elif dealerScore>17 and humanScore<21:
-        print("Player wins!" + "\n" + showCards(humanCards) + " = " + str(humanScore) + " to Dealer's " + showCards(dealerCards) + " = " + str(dealerScore))
-        quit()
+    elif dealerScore<=21 and humanScore<=21:
+        if humanScore>dealerScore:
+            print("Player wins!" + "\n"+showCards(humanCards)+" = " + str(humanScore) + " to Dealer's "+showCards(dealerCards) + " = " + str(dealerScore))
+            quit()
+        else:
+            print("Player busts with " + str(humanScore) + "\nDealer wins")
+            quit()
+
 
 
 humanScore,dealerScore  = 0,0
@@ -56,13 +63,19 @@ deck.remove(humanCardOne) #remove that card from deck
 humanCardTwo = random.choice(deck) #chose player's second card randomly
 deck.remove(humanCardTwo) #remove that card from deck
 
+
 dealerCardOne = random.choice(deck) #chose player's first card randomly
 deck.remove(dealerCardOne) #remove that card from deck
 dealerCardTwo = random.choice(deck) #chose player's second card randomly
 deck.remove(dealerCardTwo) #remove that card from deck
 
-humanCards = [humanCardOne,humanCardTwo]
 
+if humanCardOne=="A" and humanCardTwo == "A":
+    humanCardTwo = "A1"
+if dealerCardOne=="A" and dealerCardTwo=="A":
+    dealerCardTwo= "A1"
+
+humanCards = [humanCardOne,humanCardTwo]
 dealerCards = [dealerCardOne,dealerCardTwo]
 
 dealerScore = sumScores(dealerCards)
@@ -75,9 +88,17 @@ humanWins(humanScore)
 while humanScore<21:
     hitOrStand = input("Would you like to (H)it or (S)tand? ")
     if hitOrStand.upper() == "H":
-        humanCards.append(random.choice(deck))
+        human_card = optimalAce(humanCards,random.choice(deck))
+        humanCards.append(human_card)
+        #remove A if optimalAce returns A1
+        if (human_card == "A1"):
+            deck.remove("A")
+        else:
+            deck.remove(human_card)
         humanScore = sumScores(humanCards)
-        print("Player has: " + showCards(humanCards)  +" = " + str(humanScore))
+        if("A" in humanCards and humanScore>21):
+            humanScore -= 10
+        print("Player has: " + showCards(humanCards)  + " = " + str(humanScore))
         checkHumanBust(humanScore)
     elif hitOrStand.upper() == "S":
         print("Player stands with: "+showCards(humanCards) + " = " + str(humanScore))
@@ -87,15 +108,21 @@ while humanScore<21:
 checkHumanBust(humanScore)
 print()
 print("Dealer has: " + showCards(dealerCards)+ " = " + str(dealerScore))
-checkDealerBust(dealerScore,humanScore,humanCards,dealerCards)
+if(dealerScore>17):
+    checkDealerBust(dealerScore,humanScore,humanCards,dealerCards)
 
 while not dealerScore>=17:
     print("Dealer hits")
-    dealer_card = random.choice(deck)
+    dealer_card = optimalAce(dealerCards,random.choice(deck))
     dealerCards.append(dealer_card)
-    deck.remove(dealer_card)
+    if(dealer_card == "A1"):
+        deck.remove("A")
+    else:
+        deck.remove(dealer_card)
     dealerScore = sumScores(dealerCards)
     #Do optimal ace check here
+    if ("A" in dealerCards and dealerScore > 21):
+        dealerScore -= 10
     print("Dealer has: " + showCards(dealerCards) + " = " + str(dealerScore))
     if(dealerScore == 17):
         print("Dealer stands")
